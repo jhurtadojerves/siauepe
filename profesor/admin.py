ba7e8fbe9d51bc7cb3285c0f779bc559f55c93e7
Julio Hurtado <juliens@outlook.com>
2016-09-03 18:26:43 -0500

    Importar Profesores XLS

--- profesor/admin.py
+++ profesor/admin.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
+from import_export import resources, fields
+from import_export.admin import ImportMixin
 # Register your models here.
 
 from models import Profesor
+
+class ProfesorResource(resources.ModelResource):
+	class Meta:
+		model = Profesor
+		fields = ('id','nombre_completo',)
+		import_order = fields
+		IMPORT_EXPORT_SKIP_ADMIN_LOG = True
+
 @admin.register(Profesor)
-class ProfesorAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'apellido',)
+class ProfesorAdmin(ImportMixin, admin.ModelAdmin):
+	list_display = ('id','nombre_completo',)
+	resource_class = ProfesorResource
--- profesor/models.py
+++ profesor/models.py
@@ -6,8 +6,7 @@ from django.db import models
 # Create your models here.
 
 class Profesor(models.Model):
-    nombre = models.CharField(max_length=32, null=False)
-    apellido = models.CharField(max_length=32, null=False)
+    nombre_completo = models.CharField(max_length=128, null=False)
 
     def __unicode__(self):
-        return self.nombre + " " + self.apellido
+        return self.nombre_completo
