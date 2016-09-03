e3a07a911f8ccf5575f9ed07301a0563edc4ee7d
Julio Hurtado <juliens@outlook.com>
2016-09-03 16:14:14 -0500

    Archivos adicionales para heroku

--- siauepe/settings.py
+++ siauepe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'qnh2qk27ag1a387xa&(sn^#f*(sedxnrzyzn6u_@qub7zsyc)$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -131,14 +131,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT = 'https://juliohurtado.me/siauepe/'
+STATIC_URL = 'https://juliohurtado.me/siauepe/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = '/login/'
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 try:
     from .local_settings import *
