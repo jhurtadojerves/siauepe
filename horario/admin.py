e8902ef4ebadff5e2245bb9716e97a05bc9db043
Julio Hurtado <juliens@outlook.com>
2016-09-09 10:53:58 -0500

    Admin de Horario importar desde cvs

--- horario/admin.py
+++ horario/admin.py
@@ -1,13 +1,25 @@
 # -*- coding: UTF-8 -*-
 from django.contrib import admin
-
+from import_export import resources, fields
+from import_export.admin import ImportExportMixin
 from models import Horario
 
 # Register your models here.
 
+class HorarioResource(resources.ModelResource):
+	class Meta:
+		model = Horario
+		fields = ('hora', 'dia', 'cursoasignatura',)
+		import_order = fields
+		export_order = fields
+		IMPORT_EXPORT_SKIP_ADMIN_LOG = True
+
+
+
 @admin.register(Horario)
-class HorarioAdmin(admin.ModelAdmin):
+class HorarioAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ['dia', 'hora', 'curso_asignado',]
     raw_id_fields = ('cursoasignatura',)
+    class_resource = HorarioResource
 
 
