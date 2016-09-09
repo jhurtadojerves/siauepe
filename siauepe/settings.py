328b3c26a0aabadb8ce5d6cb7b2d441b9740e875
Julio Hurtado <juliens@outlook.com>
2016-09-08 22:22:31 -0500

    collect statics

--- siauepe/settings.py
+++ siauepe/settings.py
@@ -140,11 +140,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-#STATIC_ROOT = 'https://juliohurtado.me/siauepe/'
-#STATIC_URL = 'https://juliohurtado.me/siauepe/'
+STATIC_ROOT = 'https://juliohurtado.me/siauepe/'
+STATIC_URL = 'https://juliohurtado.me/siauepe/'
 
-STATIC_ROOT = '/static/'
-STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
+#STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
